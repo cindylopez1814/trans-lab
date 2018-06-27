faf7acfcb79a8536d25d27c4f7f091b9fb8e1c16
Cindy Lopez <cindy.lopezoportus@gmail.com>
2018-06-27 15:58:26 -0400

    funcion de validar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-/* Holiii acá va tu código también */
+const btnIniciar = document.getElementById('init');
+
+btnIniciar.addEventListener('click', () => { 
+  let email = document.getElementById('email').value;
+  let password = document.getElementById('password').value;
+  const expresion = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+  
+  if(email === "" || password === "") {
+    alert("Todos los campos son obligatorios");
+  }
+  if(!expresion.test(email)) {
+    alert("El correo no es valido")
+  }
+  if(password.length > 8) {
+    alert("La contaseña es muy larga");
+  }
+  if (isNaN(password)){
+    alert("La contaseña no es valida");
+    return false;
+  }
+}); 
